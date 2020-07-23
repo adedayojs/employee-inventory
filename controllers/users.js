955a3267203e721a0ed0be20169863f23a58875d
Dayo Adedunye <aadedunye@gidimobile.com>
2020-07-23 11:00:57 +0100

    (Update): Implemented Express backend files to handle http request and user detail

---
+++ controllers/users.js
@@ -0,0 +1,13 @@
+module.exports = {
+  getUser(req, res) {
+    const id = req.params.id;
+  },
+  getAllUsers(req, res) {},
+  logUserIn(req, res) {},
+
+  signUserUp(req, res) {},
+};
+
+const storage = [
+  { firstName: "Adedayo", lastName: "Adedunye", userName: "samfeolu", password: "1234", email: "samfeolu@gmail.com" },
+];
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const {getAllUsers, getUser} = require('../controllers/users')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', getAllUsers);
+router.get('/:id', getUser);
+
+//  These should be in it own router file but because of the storage mechanism. For simplicity purpose
+router.get('/login', getAllUsers);
+router.get('/signup', getAllUsers);
 
 module.exports = router;
