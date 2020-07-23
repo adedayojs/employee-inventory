3d0458387e31753195cf1187b9ac16af98db902c
adedayojs <samfeolu@gmail.com>
2020-07-23 10:54:17 +0100

    (Update): Added Angular forms for login implementation

--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -6,12 +6,14 @@ import { AuthComponent } from './auth.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [AuthComponent, LoginComponent, SignupComponent, ForgotPasswordComponent],
   imports: [
     CommonModule,
+    SharedModule,
     AuthRoutingModule
   ]
 })
--- client/src/app/auth/components/login/login.component.ts
+++ client/src/app/auth/components/login/login.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private formBuilder: FormBuilder) {}
+
+  loginForm = this.formBuilder.group({
+    email: '',
+    password: '',
+  });
 
   ngOnInit(): void {}
 
   login(): void {
-    console.log('Login');
+    console.log(this.loginForm.value);
     localStorage.setItem('userId', 'Notnull');
     localStorage.setItem('isAdmin', 'Notnull');
     this.router.navigate(['admin']);
