9bf26583292cb523baf2de2d1a4d3c265f2df62a
adedayojs <samfeolu@gmail.com>
2020-07-23 13:28:02 +0100

    (Feature):Implement Login for admin and staff

--- client/src/app/auth/components/login/login.component.ts
+++ client/src/app/auth/components/login/login.component.ts
@@ -1,25 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor(private router: Router, private formBuilder: FormBuilder) {}
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private http: HttpClient
+  ) {}
 
   loginForm = this.formBuilder.group({
     email: '',
     password: '',
   });
 
+  error = '';
+  formError = false;
+
   ngOnInit(): void {}
 
   login(): void {
     console.log(this.loginForm.value);
-    localStorage.setItem('userId', 'Notnull');
-    localStorage.setItem('isAdmin', 'Notnull');
-    this.router.navigate(['admin']);
+    this.http.post<any>('/api/v1/users/login', this.loginForm.value).subscribe(
+      (res) => {
+        console.log(res);
+        alert('Login Successful');
+        localStorage.setItem('userId', res.id);
+        const admin = res.admin
+          ? localStorage.setItem('isAdmin', 'true')
+          : null;
+        this.router.navigate(['/admin']);
+      },
+      (err) => {
+        console.log(err);
+        this.error = err.error.message;
+        this.formError = true;
+        setTimeout(() => {
+          this.formError = false;
+        }, 3000);
+        return;
+      }
+    );
   }
 }
