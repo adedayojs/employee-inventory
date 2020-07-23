418e1eab812304b997d53ca7b26d2b07ded6004f
Dayo Adedunye <aadedunye@gidimobile.com>
2020-07-23 03:09:42 +0100

    (Update): Implemented Login guard to redirect user if they are already loggedin

--- client/src/app/auth/components/login/login.component.ts
+++ client/src/app/auth/components/login/login.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
 
   login(): void {
     console.log('Login');
+    localStorage.setItem('userId', 'Notnull');
+    localStorage.setItem('isAdmin', 'Notnull');
+    this.router.navigate(['admin']);
   }
 }
