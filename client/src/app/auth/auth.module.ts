eab55cc765ec4ead84237e01a06e83ba48790dca
Dayo Adedunye <aadedunye@gidimobile.com>
2020-07-23 01:11:37 +0100

    (Setup): Added basic auth files

---
+++ client/src/app/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { AuthComponent } from './auth.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/login/signup/signup.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+
+
+@NgModule({
+  declarations: [AuthComponent, LoginComponent, SignupComponent, ForgotPasswordComponent],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule { }
